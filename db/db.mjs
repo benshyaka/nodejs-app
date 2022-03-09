bb58952bfd2ecd7a0c210f5fa50e840c45e8b02c
nkunzi llc <nkunzillc@gmail.com>
2022-03-09 12:49:57 +0200

    Adding all CRUD operations

---
+++ db/db.mjs
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const url = process.env.DBCON;
+
+const dbcon = new mongoose.connect(url)
+    .then(() => {
+        console.log('Connected to database ')
+    })
+    .catch((err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
+
+export default dbcon;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,58 @@
-var express = require('express');
-var router = express.Router();
+import express from "express";
+const router = express.Router();
+import "../db/db.mjs"
+import Blog from "../models/blog.js";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+    res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+router.post('/blogs', async(req, res) => {
+    const blogpost = req.body
+    console.log(blogpost)
+    const blog = new Blog(blogpost);
+    try {
+        await blog.save();
+        res.status(201).send(blog);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+router.get('/blogs', async(req, res) => {
+    try {
+        const blogs = await Blog.find({});
+        console.log(blogs)
+        res.status(200).send(blogs);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+router.get('/blogs/:id', async(req, res) => {
+    try {
+        const blog = await Blog.findById(req.params.id);
+        if (!blog) {
+            return res.status(404);
+        }
+        res.status(200).send(blog);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+
+router.delete('/blogs/:id', async(req, res) => {
+    try {
+        const blog = await Blog.findByIdAndDelete(req.params.id);
+        if (!blog) {
+            return res.status(404).send();
+        }
+        res.send(blog);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+export default router;
