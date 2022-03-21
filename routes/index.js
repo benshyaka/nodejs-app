3ab9e41098504cda2d686bf1141d4b9498cd0b95
nkunzi llc <nkunzillc@gmail.com>
2022-03-21 13:01:38 +0200

    Resolving status codes

--- routes/index.js
+++ routes/index.js
@@ -12,19 +12,18 @@ router.get('/', (req, res, next) => {
     res.render('index', { title: 'Express' });
 });
 
-router.post('/blogs', async(req, res) => {
+router.post('/blogs/add', async(req, res) => {
     if (req.user) {
         const blogpost = req.body
-            // console.log(blogpost)
         const blog = new Blog(blogpost);
         try {
             await blog.save();
-            res.status(201).send(blog);
+            res.status(201).json({status: "success", data: blog});
         } catch (error) {
-            res.status(500).send(error);
+            res.status(500).json({status: "failed", data: blog});
         }
     } else {
-        return res.status(401).json({ message: 'Unauthorized user! Please sign in to add new blog' });
+        return res.status(401).json({ status: "error", message:'Unauthorized user! Please sign in to add new blog'});
     }
 
 })
@@ -32,10 +31,9 @@ router.post('/blogs', async(req, res) => {
 router.get('/blogs/all', async(req, res) => {
     try {
         const blogs = await Blog.find({});
-        console.log(blogs)
-        res.status(200).send(blogs);
+        res.status(200).json({status: "success", data: blog});
     } catch (error) {
-        res.status(500).send(error);
+        res.status(500).json({ status: "error", message:'Unable to retrieve data from database'});
     }
 })
 
@@ -45,11 +43,9 @@ router.get('/blogs/single/:id', async(req, res) => {
         if (!blog) {
             return res.status(404);
         }
-        res.status(200).send(blog);
+        res.status(200).json({status: "success", data: blog});
     } catch (error) {
-        res.status(500).send("Bolg with that id can not be found");
-    }
-})
+res.status(500).json({ status: "error", message:'Unable to delete data from database, id is required'})
 
 
 router.delete('/blogs/:id', async(req, res) => {
@@ -57,48 +53,45 @@ router.delete('/blogs/:id', async(req, res) => {
         try {
             const blog = await Blog.findByIdAndDelete(req.params.id);
             if (!blog) {
-                return res.status(404).send("Bolg with that id can not be found");
+                return res.status(200).json({status: "success", data: blog});
             }
-            res.send("this blog is deleted" + blog);
+            res.status(200).json({status: "success", data: blog});
         } catch (error) {
-            res.status(500).send("Bolg with that id can not be found");
+            res.status(500).json({ status: "error", message:'Unable to delete data from database, id is required'})
         }
     } else {
-        return res.status(401).json({ message: 'Unauthorized user! Please sign in' });
+        return res.status(401).json({ status: "fail", message: 'Unauthorized user! Please sign in' });
     }
 
 })
 
-router.post('/blogs/update/:id', async(req, res) => {
+router.put('/blogs/update/:id', async(req, res) => {
     if (req.user) {
         try {
             const blogup = req.body
             const blogupdate = await Blog.findOneAndUpdate(req.params.id, blogup);
             if (!blogupdate) {
-                return "error could not update blog" + res.status(404);
+                return res.status(404).json({ status: "fail", message: 'Unable to update data to the database, id is required' });
             }
-            res.send("Blog is updated: " + blogupdate);
+            res.status(201).json({status: "success", data: blog});
         } catch (error) {
-            res.status(500).send(error);
+            res.status(500).json({ status: "fail", message: 'Unable to update data to the database' });
         }
     } else {
-        return res.status(401).json({ message: 'Unauthorized user! Please sign in' });
+        return res.status(401).json({ status: "fail", message: 'Unauthorized user! Please sign in' });
     }
 
 })
 
 router.post('/auth/register', async(req, res) => {
-    console.log(req.body)
     var newUser = new User(req.body);
     newUser.hash_password = bcrypt.hashSync(req.body.password, 10);
     newUser.save(function(err, user) {
         if (err) {
-            return res.status(400).send({
-                message: "Can't add new user " + err
-            });
-        } else {
+            return res.status(400).json({ status: "fail", message: err });
+        } else if (user) {
             user.hash_password = undefined;
-            return res.json(user);
+            return res.status(201).json({status: "success", data: user});
         }
     });
 })
@@ -107,17 +100,15 @@ router.post('/auth/signin', (req, res) => {
     const userdata = User.findOne({
         email: req.body.email
     }, async(err, user) => {
-        console.log(req.body.password, user)
-
         if (err) throw err;
         if (!user) {
             return res.status(401).json({ message: 'Authentication failed. Invalid email or password.' });
         } else {
             const match = await bcrypt.compare(req.body.password, user.hash_password);
             if (match) {
-                return res.json({ token: jwt.sign({ email: user.email, fullName: user.names, _id: user._id }, 'RESTFULAPIs') });
+                return res.status(200).json({ token: jwt.sign({ status: "success", email: user.email, fullName: user.names, _id: user._id }, 'RESTFULAPIs') });
             } else {
-                return res.status(401).json({ message: 'Authentication failed. Invalid email or password.' });
+                return res.status(401).json({ status: "fail", message: 'Authentication failed. Invalid email or password.' });
             }
         }
     });
